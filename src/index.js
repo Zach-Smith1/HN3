fe77ad2a6160f148709a5be0ed3031266d2ef8ab
Zach Smith <zachsmithglobal@icloud.com>
2023-07-18 18:23:02 -0400

    deleting

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-
-const mountNode = document.getElementById("app");
-const root = createRoot(mountNode);
-root.render(<App/>);
